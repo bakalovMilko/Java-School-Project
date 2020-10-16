37aac7a8811792289523d17134f2b754d8093edb
bakalovMilko <71628681+bakalovMilko@users.noreply.github.com>
2020-10-16 12:39:12 +0300

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -3,13 +3,71 @@ package milko;
 import java.util.Scanner;
 
 public class Sudoku {
-	public static boolean row (int[] x){
-		//String sorted, ;
-		//for(int i=; i<1){
-			
-		//}
-		//return false;
-		
+	public static boolean row (int[] x,int size){
+		boolean ans;
+		for(int i=0; i<size;i++ ){
+			ans=false;
+			for(int j=0; j<size && !ans; j++) {
+				if(i==x[j]) {
+					ans=true;
+				}
+			}
+			if(!ans) return false;
+		}
+		return true;
+	}
+	public static int[][] transponedMatrix(int[][] x,int size) {
+		int[][] ans = new int[size][size];
+		for(int i=0; i<size; i++) {
+			for(int j=0; j<size; j++) {
+				ans[i][j]=x[j][i];
+			}
+		}
+		return ans;
+	}
+	public static int[][] subMatrix (int[][] matrix, int x, int y, int sizeX, int sizeY){
+		int[][] ans = new int[sizeX][sizeY];
+		for(int i=0; i<sizeY; i++) {
+			for(int j=0; j<sizeX; j++) {
+				ans[i][j]=matrix[y+j][x+i];
+			}
+		}
+		return ans;
+	}
+	public static boolean square(int[][] x,int size) {
+		boolean ans=true;
+		for(int i=0; i<size*size; i++) {
+			ans=false;
+			for(int j=0; j<size; j++) {
+				for(int t=0; t< size; t++) {
+					if(i==x[j][t]) {
+						ans=true;
+					}
+				}
+			}
+			if(!ans) return false;
+		}
+		return true;
+	}
+	public static boolean solutionOfSudoku (int[][] x, int size) {
+		for(int i=0; i<size; i++) {
+			if(!row(x[i], size)) return false;
+		}
+		for(int i=0; i<size; i++) {
+			if(!row(transponedMatrix(x, size)[i], size)) return false;
+		}
+		for(int i=0; i<Math.sqrt(size);i++) {
+			for(int j=0; j<Math.sqrt(size);j++) {
+				if(!square(subMatrix(x, (int)(i*Math.sqrt(size)), 
+						(int)(j*Math.sqrt(size)), 
+						(int)(Math.sqrt(size)), 
+						(int)(Math.sqrt(size))),
+						(int)(Math.sqrt(size)))) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,7 +80,7 @@ public class Sudoku {
 				sudoku[i][j]=sc.nextInt();
 			}
 		}
-		
+		System.out.println((solutionOfSudoku(sudoku,size))? "Yes":"No");
 		sc.close();
 	}
 
