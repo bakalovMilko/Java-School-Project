7fb17f23313883e376686beadf61405895d247b1
bakalovMilko <71628681+bakalovMilko@users.noreply.github.com>
2020-11-05 15:35:06 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,25 @@
 package ObjectOrientedProgramming;
 
 public class Circle {
-
+	private double radius=1;
+	
+	Circle(){
+		
+	}
+	Circle(double radius){
+		this.radius=radius;
+	}
+	
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+	public double getPerimeter() {
+		return 2*radius*Math.PI;
+	}
+	public void setRadius (double newRadius) {
+		radius=newRadius;
+	}
+	public double getRadius() {
+		return radius;
+	}
 }
